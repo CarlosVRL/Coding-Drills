6d1c30e387dbdf36128384ac9bd8202291ccf3f4
Carlos <Carlos@Matrix.attlocal.net>
2017-07-13 01:18:46 -0500

    Grading Program Drill - 001

---
+++ drills/grading_program.cpp
@@ -0,0 +1,16 @@
+/*
+ * Write a program that allows the user to enter the grade scored in a programm-
+ * ing class  (0-100).  If the user  scored a 100 then notify the user that they
+ * got a perfect score.
+ * 
+ * * Modify the program  so that if the user scored a 90-100 it informs the user 
+ *   that they scored an A
+ * 
+ * ** Modify the program so that it will notify the user of their letter grade
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
